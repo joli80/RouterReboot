e30c31229f0b019b5cb8e66eb8d105dd9c394701
Johan Lindqvist <joli80@gmail.com>
2015-08-30 11:36:32 +0200

    Create git repo, first commit

---
+++ RouterRebootCmd/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RouterRebootCmd
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            try
+            {
+                string usage = "RouterRebootCmd password [ip]";
+                string ip = "192.168.0.1";
+                string password = "";
+
+                if (args.Length > 2 || args.Length < 1)
+                {
+                    Console.WriteLine(usage);
+                    throw new ArgumentException("Invalid command line arguments");
+                }
+
+                password = args[0];
+                if (args.Length == 2)
+                    ip = args[1];
+
+
+                DLinkScript.Application app = new DLinkScript.Application(ip);
+                Console.WriteLine("Logging in...");
+                app.Login(password);
+                Console.WriteLine("Logged in");
+                app.Reboot();
+                Console.WriteLine("Rebooting...");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+    }
+}
